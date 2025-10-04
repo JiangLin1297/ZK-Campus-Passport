dfcd711aee798be6731bdbbdfba62da6023f6f4c
N0b0dy-1297 <3067574089@qq.com>
2025-10-04 23:06:37 +0800

    try post

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,16 @@
-const nextConfig = {}
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+                net: false,
+                tls: false,
+            };
+        }
+        return config;
+    },
+};
+
+module.exports = nextConfig;
